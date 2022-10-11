85d7377c12b3a4591ca5fdc9926adc1b288ff9d0
jose <jrodriguez@itsolutionspcllc.com>
2022-10-10 20:15:36 -0400

    OperatorExcercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace OperatorExercise
+﻿
+
+
+namespace OperatorExercise
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        // RADIO METHOD
+        public static double radioMethod(double radius)
         {
-            Console.WriteLine("Hello, World!");
+
+            return Math.PI * (radius * radius);
+
+        }
+
+        static void Main(string [] args)
+
+        {
+
+            var a = 17;
+            var b = 4;
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {b} reminder {mod}");
+
+
+//_____________________Exercie 2 ___________________________________________________________
+
+
+            Console.WriteLine("what is the radio of your circle?");
+
+            var radius = Convert.ToDouble(Console.ReadLine());
+
+
+            var result = radioMethod(radius);
+
+            Console.WriteLine($"The area of the circle with radio of {radius} is {result}");
+            
         }
     }
 }
